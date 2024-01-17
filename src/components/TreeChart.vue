6839d45bc0c3850319a040d7aaff83543523ceb4
Miroslav_Vano <miroslav13vano@gmail.com>
2024-01-17 19:20:46 +0100

    tree

---
+++ src/components/TreeChart.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  treeData: {
+    type: Object,
+    required: true,
+    default: () => ({ label: '', children: [] })
+  }
+})
+</script>
+
+<template>
+  <OrganizationChart :value="props.treeData">
+    <template #default="slotProps">
+      <span>{{ slotProps.node.label }}</span>
+    </template>
+  </OrganizationChart>
+</template>
+
+<style>
+.p-organizationchart-lines {
+  td {
+    div {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import PrimeVue from 'primevue/config';
+import OrganizationChart from 'primevue/organizationchart';
+import 'primevue/resources/themes/lara-light-green/theme.css'
+
+
 
 const app = createApp(App)
 
-app.use(router)
+app.use(PrimeVue);
+app.component('OrganizationChart', OrganizationChart);
+
 
 app.mount('#app')
+
